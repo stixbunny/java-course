a74ef6c8627b9cf244ecfc756dd743fee1bcd972
Milenko Castillo <milenko.castillo@gmail.com>
2024-01-10 17:43:52 -0300

    section 4 step 1

---
+++ learn-spring-framework-02/src/main/java/com/udemycourse/learnspringframework/examples/d1/LazyInitializationLauncherApplication.java
@@ -0,0 +1,41 @@
+package com.udemycourse.learnspringframework.examples.d1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+class ClassA {
+
+}
+
+@Component
+@Lazy
+class ClassB {
+  private ClassA classA;
+
+  public ClassB(ClassA classA) {
+    System.out.println("Initialization Logic");
+    this.classA = classA;
+  }
+
+  public void doSomething() {
+    System.out.println("Doing something in ClassB");
+  }
+}
+
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+
+  public static void main(String[] args) {
+    try (var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)) {
+      System.out.println("Context Initialization completed");
+      context.getBean(ClassB.class).doSomething();
+    }
+  }
+}
