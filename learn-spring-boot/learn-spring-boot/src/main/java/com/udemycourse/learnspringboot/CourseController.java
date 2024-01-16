122d9229cd8675ea47444691f5732056862dd80b
Milenko Castillo <milenko.castillo@gmail.com>
2024-01-16 19:45:45 -0300

    section 5 step 4

---
+++ learn-spring-boot/learn-spring-boot/src/main/java/com/udemycourse/learnspringboot/CourseController.java
@@ -0,0 +1,19 @@
+package com.udemycourse.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+  @RequestMapping("/courses")
+  public List<Course> retrieveAllCourses() {
+    return Arrays.asList(
+        new Course(1, "Learn AWS", "udemy"),
+        new Course(2, "Learn DevOps", "udemy"));
+  }
+
+}
