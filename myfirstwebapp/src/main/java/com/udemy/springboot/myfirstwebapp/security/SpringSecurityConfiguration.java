90ebbff88358355d8320e80469e2079de3f54f5a
Milenko Castillo <milenko.castillo@gmail.com>
2024-01-23 00:19:16 -0300

    section 7 step 35

--- myfirstwebapp/src/main/java/com/udemy/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/udemy/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.udemy.springboot.myfirstwebapp.security;
 
 import java.util.function.Function;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -35,4 +39,15 @@ public class SpringSecurityConfiguration {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated());
+    http.formLogin(withDefaults());
+    http.csrf(csrf -> csrf.disable());
+    http.headers(header -> header.frameOptions(frame -> frame.disable()));
+    return http.build();
+  }
+
 }
