62f7e7fd541c57bfdceb9652caba032de5bb3a3e
Milenko Castillo <milenko.castillo@gmail.com>
2024-01-22 23:23:24 -0300

    section 7 step 33

--- myfirstwebapp/src/main/java/com/udemy/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/udemy/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -15,10 +15,20 @@ public class SpringSecurityConfiguration {
 
   @Bean
   public InMemoryUserDetailsManager createUserDetailsManager() {
+    UserDetails userDetails1 = createNewUser("udemy", "pass");
+    UserDetails userDetails2 = createNewUser("stixbunny", "pass");
+    return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+  }
+
+  private UserDetails createNewUser(String username, String password) {
     Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-    UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username("udemy").password("pass")
-        .roles("USER", "ADMIN").build();
-    return new InMemoryUserDetailsManager(userDetails);
+    UserDetails userDetails = User.builder()
+        .passwordEncoder(passwordEncoder)
+        .username(username)
+        .password(password)
+        .roles("USER", "ADMIN")
+        .build();
+    return userDetails;
   }
 
   @Bean
