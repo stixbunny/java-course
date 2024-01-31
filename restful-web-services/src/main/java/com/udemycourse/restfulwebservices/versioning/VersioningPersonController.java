41dfba821182a53299f82063f145239dd0e75608
stixbunny <milenko.castillo@gmail.com>
2024-01-31 13:25:11 -0300

    section 8 step 21

--- restful-web-services/src/main/java/com/udemycourse/restfulwebservices/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/udemycourse/restfulwebservices/versioning/VersioningPersonController.java
@@ -16,4 +16,34 @@ public class VersioningPersonController {
     return new PersonV2(new Name("Bob", "Charlie"));
   }
 
+  @GetMapping(path = "/person", params = "version=1")
+  public PersonV1 getPersonV1params() {
+    return new PersonV1("Bob Charlie");
+  }
+
+  @GetMapping(path = "/person", params = "version=2")
+  public PersonV2 getPersonV2params() {
+    return new PersonV2(new Name("Bob", "Charlie"));
+  }
+
+  @GetMapping(path = "/person", headers = "X-API-VERSION=1")
+  public PersonV1 getPersonV1headers() {
+    return new PersonV1("Bob Charlie");
+  }
+
+  @GetMapping(path = "/person", headers = "X-API-VERSION=2")
+  public PersonV2 getPersonV2headers() {
+    return new PersonV2(new Name("Bob", "Charlie"));
+  }
+
+  @GetMapping(path = "/person", produces = "application/vnd.company.app-v1+json")
+  public PersonV1 getPersonV1media() {
+    return new PersonV1("Bob Charlie");
+  }
+
+  @GetMapping(path = "/person", produces = "application/vnd.company.app-v2+json")
+  public PersonV2 getPersonV2media() {
+    return new PersonV2(new Name("Bob", "Charlie"));
+  }
+
 }
