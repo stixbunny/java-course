6ab5fbe4ca4ce028eec8b533e0ce964af4720fce
stixbunny <milenko.castillo@gmail.com>
2024-01-31 12:54:08 -0300

    section 8 step 19

--- restful-web-services/src/main/java/com/udemycourse/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/udemycourse/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.udemycourse.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloWorldController {
+
+  private MessageSource messageSource;
+
+  public HelloWorldController(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
 
   @GetMapping(path = "hello-world")
   // @RequestMapping(path = "hello-world", method = RequestMethod.GET)
@@ -22,6 +31,13 @@ public class HelloWorldController {
   @GetMapping("hello-world/path-variable/{name}")
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Henlo word %s", name));
+  }
+
+  @GetMapping("/hello-world-i18n")
+  public String getMethodName() {
+    Locale locale = LocaleContextHolder.getLocale();
+    return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
   }
 
 }
