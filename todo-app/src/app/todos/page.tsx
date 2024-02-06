b7b88a28b446261e0a5637aca92041f885e4f650
stixbunny <milenko.castillo@gmail.com>
2024-02-05 22:16:11 -0300

    section 11 step 13 mid

--- todo-app/src/app/todos/page.tsx
+++ todo-app/src/app/todos/page.tsx
@@ -6,22 +6,22 @@ export default function Todos() {
 
   return (
     <div className="">
-      <table className="mx-auto">
+      <table className="mx-auto table-auto border-collapse border border-slate-500">
         <thead>
           <tr>
-            <td>id</td>
-            <td>description</td>
-            <td>done?</td>
-            <td>target date</td>
+            <td className="border border-slate-500 p-3 bg-slate-600">id</td>
+            <td className="border border-slate-500 p-3 bg-slate-600">description</td>
+            <td className="border border-slate-500 p-3 bg-slate-600">done?</td>
+            <td className="border border-slate-500 p-3 bg-slate-600">target date</td>
           </tr>
         </thead>
         <tbody>
           {todos.map((t) => (
             <tr key={t.id}>
-              <td>{t.id}</td>
-              <td>{t.description}</td>
-              <td>{t.done}</td>
-              <td>{t.targetDate.toLocaleDateString("es-CL")}</td>
+              <td className="border border-slate-500 p-3 bg-slate-700">{t.id}</td>
+              <td className="border border-slate-500 p-3 bg-slate-700">{t.description}</td>
+              <td className="border border-slate-500 p-3 bg-slate-700">{t.done}</td>
+              <td className="border border-slate-500 p-3 bg-slate-700">{t.targetDate.toLocaleDateString("es-CL")}</td>
             </tr>
           ))}
         </tbody>
