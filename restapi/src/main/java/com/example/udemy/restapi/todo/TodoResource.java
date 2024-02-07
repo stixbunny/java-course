3fb23bcb1be18648f04db1c25b8b147286a3fc99
stixbunny <milenko.castillo@gmail.com>
2024-02-07 17:40:47 -0300

    section 12 step 9

--- restapi/src/main/java/com/example/udemy/restapi/todo/TodoResource.java
+++ restapi/src/main/java/com/example/udemy/restapi/todo/TodoResource.java
@@ -2,6 +2,9 @@ package com.example.udemy.restapi.todo;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,18 @@ public class TodoResource {
   @GetMapping("users/{username}/todos")
   public List<Todo> retrieveTodos(@PathVariable String username) {
     return todoService.findByUsername(username);
+  }
+
+  @GetMapping("users/{username}/todos/{id}")
+  public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
+    return todoService.findById(id);
+  }
+
+  @DeleteMapping("users/{username}/todos/{id}")
+  public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+    todoService.deleteById(id);
+    //Return No Content (204)
+    return ResponseEntity.noContent().build();
   }
 
 }
