aa403ec7de6e7337a15317e6a41e89d4016c035f
stixbunny <milenko.castillo@gmail.com>
2024-02-07 16:15:32 -0300

    section 12 step 4

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,5 +1,10 @@
 "use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [message, setMessage] = useState("");
+
   async function callApi() {
     console.log("calling api...");
     try {
@@ -9,19 +14,22 @@ export default function Home() {
         throw new Error(`${response.status}: ${response.statusText}`);
       }
       console.log("API called successfully");
+      const json = await response.json();
+      setMessage(json.message);
     } catch (error) {
       console.log("There was an error calling the API");
       console.log(error);
     }
   }
   return (
-    <div className="flex flex-col items-center min-h-[100%] justify-center">
+    <div className="flex flex-col gap-4 items-center min-h-[100%] justify-center">
       <button
         className="py-2 px-4 rounded text-text font-bold bg-primary hover:bg-primaryhover"
         onClick={callApi}
       >
         Call API
       </button>
+      <p className="font-bold italic">{message}</p>
     </div>
   );
 }
