d2e2406673d398ca603e5e02bbd2d31c619e57b1
stixbunny <milenko.castillo@gmail.com>
2024-02-07 15:47:44 -0300

    section 12 step 3

--- restapi/src/main/java/com/example/udemy/restapi/RestApiApplication.java
+++ restapi/src/main/java/com/example/udemy/restapi/RestApiApplication.java
@@ -2,12 +2,26 @@ package com.example.udemy.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping(("/**")).allowedMethods("*").allowedOrigins("http://localhost:3000");
+				WebMvcConfigurer.super.addCorsMappings(registry);
+			}
+		};
 	}
 
 }
