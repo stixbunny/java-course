fa880178a8643da0124bca8ed276e239eccbb167
stixbunny <milenko.castillo@gmail.com>
2024-02-13 19:21:43 -0300

    section 15 step 1

---
+++ mockito-demo/src/main/java/com/udemy/mockito/mockitodemo/business/SomeBusinessImpl.java
@@ -0,0 +1,20 @@
+package com.udemy.mockito.mockitodemo.business;
+
+public class SomeBusinessImpl {
+  private DataService dataService;
+
+  public int findTheGreatestFromAllData() {
+    int[] data = dataService.retrieveAllData();
+    int greatestValue = Integer.MIN_VALUE;
+    for (int value : data) {
+      if (value > greatestValue) {
+        greatestValue = value;
+      }
+    }
+    return greatestValue;
+  }
+}
+
+interface DataService {
+  int[] retrieveAllData();
+}
