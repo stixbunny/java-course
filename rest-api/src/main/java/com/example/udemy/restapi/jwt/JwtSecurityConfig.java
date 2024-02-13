2d30037235e0cc2d68da305879ed41e6f66043af
stixbunny <milenko.castillo@gmail.com>
2024-02-13 10:56:35 -0300

    section 13 step 2

--- rest-api/src/main/java/com/example/udemy/restapi/jwt/JwtSecurityConfig.java
+++ rest-api/src/main/java/com/example/udemy/restapi/jwt/JwtSecurityConfig.java
@@ -51,7 +51,7 @@ public class JwtSecurityConfig {
     return httpSecurity
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/authenticate").permitAll()
-            // .requestMatchers(PathRequest.toH2Console()).permitAll() // h2-console is a
+            .requestMatchers(PathRequest.toH2Console()).permitAll() // h2-console is a
             // servlet and NOT recommended for a
             // // production
             // .requestMatchers(HttpMethod.OPTIONS, "/**")
---
+++ rest-api/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into todo (id, description, done, target_date, username) values (100001, 'Learn AWS', false, CURRENT_DATE(), 'stixbunny');
+insert into todo (id, description, done, target_date, username) values (100002, 'Learn DevOps', false, CURRENT_DATE(), 'stixbunny');
+insert into todo (id, description, done, target_date, username) values (100003, 'Learn Golang', false, CURRENT_DATE(), 'stixbunny');
+insert into todo (id, description, done, target_date, username) values (100004, 'Learn Flask', false, CURRENT_DATE(), 'stixbunny');
+insert into todo (id, description, done, target_date, username) values (100005, 'Learn GCP', false, CURRENT_DATE(), 'stixbunny');
+insert into todo (id, description, done, target_date, username) values (100006, 'Get a degree', false, CURRENT_DATE(), 'stixbunny');
+insert into todo (id, description, done, target_date, username) values (100007, 'Do the dishes', false, CURRENT_DATE(), 'stixbunny');
