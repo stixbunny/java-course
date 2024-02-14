f2e4ae9ab9b70b12c67cd3d5dba50b4b6abc5a58
stixbunny <milenko.castillo@gmail.com>
2024-02-14 17:00:06 -0300

    section 16 step 12

--- learn-spring-security/src/main/java/com/udemy/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/udemy/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,7 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -20,5 +26,34 @@ public class BasicAuthSecurityConfiguration {
 
     return http.build();
   }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername("stixbunny")
+        .password("{noop}password")
+        .roles("USER")
+        .build();
+
+    UserDetails admin = User.withUsername("admin")
+        .password("{noop}password")
+        .roles("ADMIN")
+        .build();
+
+    return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  // @Bean
+  // public WebMvcConfigurer corsConfigurer() {
+  // return new WebMvcConfigurer() {
+
+  // @Override
+  // public void addCorsMappings(CorsRegistry registry) {
+  // registry.addMapping("/**")
+  // .allowedMethods("*")
+  // .allowedOrigins("http://localhost:3000");
+  // }
+
+  // };
+  // }
 
 }
