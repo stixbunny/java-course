c968296a397be5ca8cbd585489eeb11d8d2cd0b2
stixbunny <milenko.castillo@gmail.com>
2024-02-16 20:04:14 -0300

    section 16 step 23

---
+++ learn-oauth/src/main/java/com/udemy/learnoauth/LearnOauthApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.learnoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOauthApplication.class, args);
+	}
+
+}
