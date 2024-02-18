850a424ccf630a6f75d883fa1241e251c446f90d
stixbunny <milenko.castillo@gmail.com>
2024-02-18 17:56:52 -0300

    se tion 17 step 4

---
+++ learn-spring-aop/src/main/java/com/udemy/learnspringaop/business/BusinessService1.java
@@ -0,0 +1,21 @@
+package com.udemy.learnspringaop.business;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.udemy.learnspringaop.data.DataService1;
+
+@Service
+public class BusinessService1 {
+  private DataService1 dataService1;
+
+  public BusinessService1(DataService1 dataService1) {
+    this.dataService1 = dataService1;
+  }
+
+  public int calculateMax() {
+    int[] data = dataService1.retrieveData();
+    return Arrays.stream(data).max().orElse(0);
+  }
+}
