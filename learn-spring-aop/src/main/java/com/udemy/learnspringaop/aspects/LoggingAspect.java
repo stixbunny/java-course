123a9016bb217780f17d94025a31b560f05999ab
stixbunny <milenko.castillo@gmail.com>
2024-02-19 18:14:09 -0300

    section 17 step 7

--- learn-spring-aop/src/main/java/com/udemy/learnspringaop/aspects/LoggingAspect.java
+++ learn-spring-aop/src/main/java/com/udemy/learnspringaop/aspects/LoggingAspect.java
@@ -1,6 +1,11 @@
 package com.udemy.learnspringaop.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +16,29 @@ public class LoggingAspect {
 
   private Logger logger = LoggerFactory.getLogger(getClass());
 
-  public void logMethodCall() {
-    logger.info("Method is called: {}");
+  @Before("execution(* com.udemy.learnspringaop.*.*.*(..) )")
+  public void logMethodCallBeforeExecution(JoinPoint joinPoint) {
+    logger.info("Before Aspect - {} is called with arguments: {}", joinPoint, joinPoint.getArgs());
+  }
+
+  @After("execution(* com.udemy.learnspringaop.*.*.*(..) )")
+  public void logMethodCallAfterExecution(JoinPoint joinPoint) {
+    logger.info("After Aspect - {} has executed", joinPoint);
+  }
+
+  @AfterThrowing(
+    pointcut = "execution(* com.udemy.learnspringaop.*.*.*(..) )",
+    throwing = "exception"
+    )
+  public void logMethodCallAfterThrowing(JoinPoint joinPoint, Exception exception) {
+    logger.info("After Throwing Aspect - {} has thrown an exception {}", joinPoint, exception);
+  }
+
+  @AfterReturning(
+    pointcut = "execution(* com.udemy.learnspringaop.*.*.*(..) )",
+    returning = "result"
+    )
+  public void logMethodCallAfterReturning(JoinPoint joinPoint, Object result) {
+    logger.info("After Returning - {} has returned {}", joinPoint, result);
   }
 }
